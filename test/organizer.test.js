a33e2832ffaba24099cd48bceb69d352f7922c3d
Axel Hernández Ferrera <axelhzf@gmail.com>
2014-04-29 12:28:32 +0100

    adding co in testing

--- test/organizer.test.js
+++ test/organizer.test.js
@@ -3,7 +3,9 @@ var expect = require("chai").expect;
 var organizer = require("../src/organizer");
 var temp = require("temp");
 var async = require("async");
-
+var co = require("co");
+var thunkify = require("thunkify");
+var _ = require("underscore");
 
 describe("Organizer", function () {
 
@@ -19,50 +21,53 @@ describe("Organizer", function () {
     var tempPath;
 
     beforeEach(function (done) {
-      createTemporaryDestPath(done);
+      co(function* () {
+        yield createTemporaryDestPath();
+      })(done);
     });
 
     it("should move files", function (done) {
-      async.series([
-        testMoveFile("Game.of.Thrones.S04E01.720p.HDTV.x264-KILLERS.mkv", dest[0]),
-        testMoveFile("asdlfkjasdfñGaMe.Of.ThroNes.S04E01.720p.HDTV.x264-KILLERS.mkv", dest[0]),
-        testMoveFile("Two.and.a.Half.Men.S11E02.720p.HDTV.X264-DIMENSION", dest[3]),
-        testMoveFile("Mom.S01E20.HDTV.x264-LOL.mp4", dest[2]),
-        testMoveFile("Spiderman", undefined)
-      ], done);
+      co(function* () {
+        var tests = [
+          testMoveFile("Game.of.Thrones.S04E01.720p.HDTV.x264-KILLERS.mkv", dest[0]),
+          testMoveFile("asdlfkjasdfñGaMe.Of.ThroNes.S04E01.720p.HDTV.x264-KILLERS.mkv", dest[0]),
+          testMoveFile("Two.and.a.Half.Men.S11E02.720p.HDTV.X264-DIMENSION", dest[3]),
+          testMoveFile("Mom.S01E20.HDTV.x264-LOL.mp4", dest[2]),
+          testMoveFile("Spiderman", undefined)
+        ];
+        yield tests;
+      })(done);
     });
 
-    function testMoveFile (fileName, expectedPath) {
-      return function (cb) {
-        createTemporaryFile(fileName);
-        organizer.move(fileName, tempPath, function (err, result) {
-          if (expectedPath) {
-            expect(result).to.eql(tempPath + "/" + expectedPath + "/" + fileName);
-          } else {
-            expect(result).is.undefined;
-          }
-          cb();
-        });
-      }
+    var move = thunkify(organizer.move);
+    var mkTempDir = thunkify(temp.mkdir);
+    var mkdir = thunkify(fs.mkdir);
+    var fsOpen = thunkify(fs.open);
+    var fsClose = thunkify(fs.close);
+
+    function* testMoveFile (fileName, expectedPath) {
+      var temporaryFile = createTemporaryFile(fileName);
+      var result = yield move(temporaryFile, tempPath);
+      var expected = _.isUndefined(expectedPath) ? undefined : tempPath + "/" + expectedPath + "/" + fileName;
+      expect(result).to.equal(expected);
     }
 
-    function createTemporaryDestPath (cb) {
-      temp.mkdir("move", function (err, info) {
-        tempPath = info;
-
-        var absolutePaths = dest.map(function (directory) {
-          return tempPath + "/" + directory;
-        });
-
-        absolutePaths.forEach(fs.mkdirSync);
-
-        cb(null, absolutePaths);
+    function* createTemporaryDestPath () {
+      tempPath = yield mkTempDir("move");
+      var absolutePaths = dest.map(function (directory) {
+        return tempPath + "/" + directory;
       });
+      yield absolutePaths.map(function (path) {
+        return mkdir(path)
+      });
+      return absolutePaths;
     }
 
     function createTemporaryFile (name) {
+      var fullPath = tempPath + "/" + name;
       var fd = fs.openSync(tempPath + "/" + name, 'w');
       fs.close(fd);
+      return fullPath;
     }
   });
 
